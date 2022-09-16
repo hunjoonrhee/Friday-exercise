4ad5a6bf8057a025d75b6a9ab69a63c49acd4bbb
hunjoonrhee <hunjoonrhee90@gmail.com>
2022-09-16 13:53:43 +0200

    Add @GetMapping

--- src/main/java/de/neuefische/backend/controller/ToDoController.java
+++ src/main/java/de/neuefische/backend/controller/ToDoController.java
@@ -3,6 +3,7 @@ package de.neuefische.backend.controller;
 import de.neuefische.backend.model.ToDo;
 import de.neuefische.backend.service.ToDoService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -18,7 +19,7 @@ public class ToDoController {
     public ToDoController(ToDoService service) {
         this.service = service;
     }
-
+    @GetMapping
     public List<ToDo> getAllToDos(){
         return service.getAllToDos();
     }
